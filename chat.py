3914f862e9088df4c1a89b0c585ac912704b91c5
danielndahi <danielndahi67@gmail.com>
2024-08-14 14:15:43 +0300

    binding of port

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -32,4 +33,5 @@ def process_query(query):
     return "Sorry, I didn't understand your query."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get('PORT',8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
